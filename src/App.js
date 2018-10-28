e754f22c4165e523395848b55483ddc44766b723
yadavsunil00001 <yadavsunil00001@gmail.com>
2018-10-28 08:20:03 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -6,20 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="card" style={{'width': '18px'}}>
+  <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
       </div>
     );
   }
